2926d0086a9200dc0bfa18278db392b4d94f2deb
cjg9040 <chl8273@naver.com>
2020-02-20 18:02:29 +0900

    ssr 설정

--- plugins/pwa.js
+++ plugins/pwa.js
@@ -1,12 +1,10 @@
-if (process.client) {
-  window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.prompt();
-  });
-  // window.addEventListener('install', (e) => {
-  //   // Prevent Chrome 67 and earlier from automatically showing the prompt
-  //   e.preventDefault();
-  // });
-}
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.prompt();
+});
+// window.addEventListener('install', (e) => {
+//   // Prevent Chrome 67 and earlier from automatically showing the prompt
+//   e.preventDefault();
+// });
 
 
