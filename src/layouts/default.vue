0fc940c3cd54570d98c4515861e88044435200c8
cjg9040 <chl8273@naver.com>
2020-02-21 18:32:31 +0900

    folder 구조 변경

---
+++ src/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+  @import url("//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css");
+
+  html, body {
+    margin: 0;
+  }
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <img src="../assets/Ucut.in.svg"/>
+      <p>shorten url, share link, take pics</p>
+      <p>By clicking SHORTEN IT!, you are agreeing to Ucut.in’s Terms of Service and Privacy policy</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style>
+  .wrapper {
+    padding: 24px;
+  }
+</style>
